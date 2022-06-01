56a40dbfc5174fb7bd7fd4691c44e7a27e7ed49f
QuangMinh <93485271+QuangMinh8699@users.noreply.github.com>
2022-06-01 15:28:30 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <>
+      <div className="App">Hello, Froggy Gaming</div>
+    </>
+  );
+};
+
+export default App;
